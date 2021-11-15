bda53c604337d97e1db9ff9ce7379fa66d8a9ee6
Roger <guilhermemoura403@gamil.com>
2021-11-15 14:20:41 -0300

    Estudos Python

---
+++ variaveis.py
@@ -0,0 +1,21 @@
+#variaveis numericas em python em python
+x = 10 
+print(x)
+y = 10.5
+
+#strings em python
+cidade = "São Paulo"
+print(cidade)
+
+#variaveis booleanas
+disponivel = True
+print(disponivel)
+
+#verificando tipo de variavel
+print(type(y))
+print(cidade, type(cidade))
+print(disponivel,type(disponivel))
+#type int = inteiro // type float = decimal
+#type bool = booleano
+
+
